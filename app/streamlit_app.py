c2d9ab13b22e77d77d04c1d29388134372b491f0
Bruno Baraviera <bruno.baraviera@outlook.com>
2026-03-03 15:36:32 -0300

    Melhorias no UX do frontend

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
 from src.data_loader import load_movies
-from src.content_model import recommend_movies
-from src.collaborative_model import recommend_movies_collaborative
-from src.deep_model import recommend_movies_deep
 
 def disable():
   st.session_state.loaded = True
@@ -13,11 +10,11 @@ def reset():
   st.session_state.selection = None
 
 st.set_page_config(
-  page_title = "Recomendador de filmes",
+  page_title = "Recomendação de filmes",
   layout = "centered"
 )
 
-st.title("Recomendador de Filmes", text_alignment = "center")
+st.title("Recomendação de Filmes", text_alignment = "center")
 
 if not "selection" in st.session_state:
   st.session_state.selection = None
@@ -25,7 +22,8 @@ if not "selection" in st.session_state:
 if not "loaded" in st.session_state:
   st.session_state.loaded = False
 
-movies = load_movies()
+with st.spinner("Carregando..."):
+  movies = load_movies()
 
 movie_titles = movies["title"].sort_values().unique()
 
@@ -38,23 +36,31 @@ selected_movie = st.selectbox(
   placeholder = "Selecione uma opção"
 )
 
-request_button = st.button("Recomendar", disabled = st.session_state.loaded, on_click = disable)
+request_button = st.button("Ver recomendações", disabled = st.session_state.loaded, on_click = disable)
 
 if request_button:
+  with st.spinner("Carregando..."):
+    from src.content_model import recommend_movies
+    from src.collaborative_model import recommend_movies_collaborative
+    from src.deep_model import recommend_movies_deep
+    
   st.header(f"Recomendações para: {selected_movie}")
 
   st.markdown("### Modelo de recomendação por gênero")
-  recs = recommend_movies(selected_movie)
+  with st.spinner("Carregando..."):
+    recs = recommend_movies(selected_movie)
   for movie in recs:
     st.write(movie)
 
   st.markdown("### Modelo de recomendação colaborativo")
-  recs = recommend_movies_collaborative(selected_movie)
+  with st.spinner("Carregando..."):
+    recs = recommend_movies_collaborative(selected_movie)
   for movie in recs:
     st.write(movie)
 
   st.markdown("### Modelo de recomendação com Deep Learning")
-  recs = recommend_movies_deep(selected_movie)
+  with st.spinner("Carregando..."):
+    recs = recommend_movies_deep(selected_movie)
   for movie in recs:
     st.write(movie)
 
